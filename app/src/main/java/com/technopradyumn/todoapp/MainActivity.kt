cc244d777c20d2194d709bdc1e9f1bea29168ae0
IRFAN <irfankadeer.cgc@gmail.com>
2024-04-30 00:00:26 +0530

    TodoAPP

---
+++ app/src/main/java/com/technopradyumn/todoapp/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.technopradyumn.todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.technopradyumn.todoapp.screens.HomeScreen
+import com.technopradyumn.todoapp.ui.theme.TodoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            TodoAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SetStatusBarColor(
+                        statusBarColor = Color.White,
+                        contentColor = Color.Black
+                    )
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SetStatusBarColor(statusBarColor: Color, contentColor: Color) {
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = statusBarColor,
+            darkIcons = contentColor == Color.Black
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun HomeScreenPreview() {
+    TodoAppTheme {
+        HomeScreen()
+    }
+}
